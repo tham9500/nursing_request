427270acfc8d7a2a24695b41b3bc27ae9e815860
Tham M2 <tham.sal@freewillfx.com>
2024-10-19 17:41:50 +0700

    add intro modules

---
+++ lib/views/intro/controller/intro_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class IntroController extends GetxController {
+  var totalPage = 4.obs;
+  var currentPage = 0.obs;
+}
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// ignore: unused_import
+import 'package:nursing_request/constant/secure_storage.dart';
 import 'package:nursing_request/constant/value_constant.dart';
 import 'package:nursing_request/controller/app_info_controller.dart';
+import 'package:nursing_request/views/intro/view/intro_page.dart';
 import 'package:nursing_request/widgets/custom_text.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,6 +16,32 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   final AppInfoController _appInfoController = Get.put(AppInfoController());
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _appInfoController.getDeviceInfo();
+    _delay();
+  }
+
+  _delay() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    // String? intro = await getInitApp();
+    // String? login = await getLogIn();
+
+    // if (intro == null) {
+    Get.offAll(() => const IntroPage());
+    // } else {
+    //   if (login != null) {
+    //     Get.offAll(() => const CustomBottomNav());
+    //   } else {
+    //     Get.offAll(() => const LoginPage());
+    //   }
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +51,29 @@ class _SplashScreenState extends State<SplashScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              height: 72.0,
-              width: 72.0,
+              padding: const EdgeInsets.all(margin),
+              height: 96.0,
+              width: 96.0,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(margin),
                 color: Colors.pink.shade300,
               ),
+              child: Image.asset('assets/logo/nurse.png'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const CustomText(
+              'Nurse Progress',
+              size: fontSizeXXL,
+              weight: FontWeight.bold,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: marginX6),
+              child: LinearProgressIndicator(),
             ),
             const SizedBox(
               height: 8,
